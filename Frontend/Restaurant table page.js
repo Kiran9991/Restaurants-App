f3742a9a2b671d69e07cacd93f45f4459eebdcf9
Kiran9991 <122242670+Kiran9991@users.noreply.github.com>
2023-04-05 13:00:43 +0530

    Completed the project

--- Frontend/Restaurant table page.js
+++ Frontend/Restaurant table page.js
@@ -8,62 +8,66 @@ submitbtn.addEventListener('click', storeTableDetails);
 function storeTableDetails(e) {
     e.preventDefault();
 
-    let choosePrice = document.getElementById('price').value;
-    let chooseDish = document.getElementById('dish').value;
-    let chooseTable = document.getElementById('table').value;
+    let price = document.getElementById('price').value;
+    let dish = document.getElementById('dish').value;
+    let table = document.getElementById('table').value;
 
     let orderDetails = {
-        choosePrice,
-        chooseDish,
-        chooseTable
+        price,
+        dish,
+        table
     }
-    showTableDetails(orderDetails)
+    // showTableDetails(orderDetails)
 
-    axios.post('https://crudcrud.com/api/a9083fd77dbb4c12b0a5ffdf19427f6b/orderDetails', orderDetails)
+    axios.post('http://localhost:5000/order/add-order', orderDetails)
     .then((response) => {
-        showTableDetails(response.data)
+        showTableDetails(response.data.newOrderDetail)
         console.log(response.data)
+    }).catch((err) => {
+        document.body.innerHTML = document.body.innerHTML+`<h4>Something went Wrong</h4>`;
+        console.log(err);
     })
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    axios.get('https://crudcrud.com/api/a9083fd77dbb4c12b0a5ffdf19427f6b/orderDetails').then((response) => {
-        for(let i=0; i<response.data.length; i++) {
-            showTableDetails(response.data[i]);
+    axios.get('http://localhost:5000/order/get-orders').then((response) => {
+        for(let i=0; i<response.data.allOrderDetails.length; i++) {
+            showTableDetails(response.data.allOrderDetails[i]);
         }
-    })
+    }).catch((error) => console.log(error));
 })
 
 function showTableDetails(orderDetails) {
     let li = document.createElement('li');
-    li.textContent = `${orderDetails.choosePrice} - ${orderDetails.chooseTable} - ${orderDetails.chooseDish}`;
+    li.textContent = `${orderDetails.price} - ${orderDetails.dish} - ${orderDetails.table}`;
+
 
     let deletbtn = document.createElement('input');
     deletbtn.type = 'button';
     deletbtn.value = 'Delete';
 
     function delteTableId(tableId) {
-        axios.delete(`https://crudcrud.com/api/a9083fd77dbb4c12b0a5ffdf19427f6b/orderDetails/${tableId}`)
+        axios.delete(`http://localhost:5000/order/delete-order/${tableId}`)
         .then((res) => console.log(res))
     }
 
     li.appendChild(deletbtn);
 
     deletbtn.onclick = () => {
-        if(orderDetails.chooseTable==='Table 1') {
+        if(orderDetails.table==='Table 1') {
             Table1.removeChild(li);
-            delteTableId(orderDetails._id);
-        }else if(orderDetails.chooseTable==='Table 2') {
+            delteTableId(orderDetails.id);
+        }else if(orderDetails.table==='Table 2') {
             Table2.removeChild(li)
-            delteTableId(orderDetails._id);
+            delteTableId(orderDetails.id);
         }else {
             Table3.removeChild(li)
-            delteTableId(orderDetails._id);
+            delteTableId(orderDetails.id);
         }
     }
-    if(orderDetails.chooseTable==='Table 1') {
+    if(orderDetails.table==='Table 1') {
         Table1.appendChild(li);
-    }else if(orderDetails.chooseTable==='Table 2') {
+    }else if(orderDetails.table==='Table 2') {
         Table2.appendChild(li);
     }else {
         Table3.appendChild(li);
